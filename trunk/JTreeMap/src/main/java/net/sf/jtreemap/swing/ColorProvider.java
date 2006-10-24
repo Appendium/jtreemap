e979dc944408c658b5b43ccb5f3138d65fd0db1e
benoitx <benoitx@0da2067e-fa1f-0410-9d0f-9a515117b8f7>
2006-10-24 22:19:05 +0000

    Added Header & Footer

--- trunk/JTreeMap/src/main/java/net/sf/jtreemap/swing/ColorProvider.java
+++ trunk/JTreeMap/src/main/java/net/sf/jtreemap/swing/ColorProvider.java
@@ -1,5 +1,5 @@
 /*
- * ObjectLab, http://www.objectlab.co.uk/open is sponsoring the ObjectLab Kit.
+ * ObjectLab, http://www.objectlab.co.uk/open is supporting JTreeMap.
  * 
  * Based in London, we are world leaders in the design and development 
  * of bespoke applications for the securities financing markets.
@@ -62,3 +62,19 @@ public abstract class ColorProvider implements Serializable {
      */
     public abstract JPanel getLegendPanel();
 }
+/*
+ *                 ObjectLab is supporing JTreeMap
+ * 
+ * Based in London, we are world leaders in the design and development 
+ * of bespoke applications for the securities financing markets.
+ * 
+ * <a href="http://www.objectlab.co.uk/open">Click here to learn more about us</a>
+ *           ___  _     _           _   _          _
+ *          / _ \| |__ (_) ___  ___| |_| |    __ _| |__
+ *         | | | | '_ \| |/ _ \/ __| __| |   / _` | '_ \
+ *         | |_| | |_) | |  __/ (__| |_| |__| (_| | |_) |
+ *          \___/|_.__// |\___|\___|\__|_____\__,_|_.__/
+ *                   |__/
+ *
+ *                     www.ObjectLab.co.uk
+ */
