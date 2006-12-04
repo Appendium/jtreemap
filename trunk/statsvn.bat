5e5b7d0abb273dc0578c0db1a95aa3253c2552cb
benoitx <benoitx@0da2067e-fa1f-0410-9d0f-9a515117b8f7>
2006-12-04 00:18:10 +0000

    modified the project and module for QALab 1.1

--- trunk/statsvn.bat
+++ trunk/statsvn.bat
@@ -1,3 +1,3 @@
-rem svn log -v --xml > logfile.log
+svn log -v --xml > logfile.log
 rem mkdir qalab\target\docs\statsvn
 java -jar c:\java\statsvn\statsvn.jar -format xdoc -verbose -output-dir src\site\statsvn -title JTreeMap -viewvc http://svn.sourceforge.net/viewvc/jtreemap/trunk ./logfile.log .
