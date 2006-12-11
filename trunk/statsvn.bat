50728cc336a29887431fc1f8ea5444ca912fce20
benoitx <benoitx@0da2067e-fa1f-0410-9d0f-9a515117b8f7>
2006-12-11 12:25:25 +0000

    site built

--- trunk/statsvn.bat
+++ trunk/statsvn.bat
@@ -1,3 +1,3 @@
-svn log -v --xml > logfile.log
+svn log -v --xml https://svn.sourceforge.net/svnroot/jtreemap/ > logfile.log
 rem mkdir qalab\target\docs\statsvn
-java -jar c:\java\statsvn\statsvn.jar -format xdoc -verbose -output-dir src\site\statsvn -title JTreeMap -viewvc http://svn.sourceforge.net/viewvc/jtreemap/trunk ./logfile.log .
+java -jar c:\java\statsvn\statsvn.jar -exclude "**/qalab.xml" -tags "^1.1.0|^1.0.0" -format xdoc -verbose -output-dir src\site\statsvn -title JTreeMap -viewvc http://svn.sourceforge.net/viewvc/jtreemap/trunk ./logfile.log .
