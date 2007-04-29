7a389f216f7e3e5a39988538bf451824f7e592e4
Benoit Xhenseval <benoitx@users.noreply.github.com>
2007-04-29 09:34:51 +0000

    relocated svn repo.

--- statsvn.bat
+++ statsvn.bat
@@ -1,3 +1,2 @@
-svn log -v --xml https://svn.sourceforge.net/svnroot/jtreemap/ > logfile.log
-rem mkdir qalab\target\docs\statsvn
+svn log -v --xml https://jtreemap.svn.sourceforge.net/svnroot/jtreemap/ > logfile.log
 java -jar c:\java\statsvn\statsvn.jar -exclude "**/qalab.xml" -tags "^1.1.0|^1.0.0" -xdoc -config-file statsvn.properties -verbose -output-dir src\site\statsvn -title JTreeMap -viewvc http://svn.sourceforge.net/viewvc/jtreemap/trunk ./logfile.log .
